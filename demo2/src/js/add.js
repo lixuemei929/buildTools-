b988bae000393341bfc3d76e82c97dcbc315b644
lixuemei929 <498714373@qq.com>
2017-05-31 08:47:01 +0800

    test about webpack

---
+++ demo2/src/js/add.js
@@ -0,0 +1,8 @@
+require ('../css/style.css');
+//require ("style-loader!css-loader!../css/style.css"); //(内嵌式)
+var hs=document.createElement('h2');
+    hs.innerHTML='1505FE.B';
+    hs.className='title';
+    document.body.appendChild(hs);
+
+
---
+++ demo2/webpack.config.js
@@ -0,0 +1,37 @@
+﻿var path=require('path');
+var webpack=require('webpack');
+var HtmlWebpackPlugIn = require("html-webpack-plugin");
+
+module.exports={
+    //入口文件
+    entry:'./src/js/app.js',
+    /*生成的文件*/
+    output:{
+        path:path.join(__dirname,"/dist"),
+        filename:'js/move.js'
+    },
+    module:{
+        loaders:[
+            {test:/.css$/,loader:'style-loader!css-loader'}
+        ]
+    },
+    plugins:[  //添加第三方插件
+       new HtmlWebpackPlugIn({
+           template:"./index.html",
+           filename:'./default.html',
+           inject:true,
+           hash:true,
+           minify:{//压缩设置
+               removeComments:true,  //删除注释
+               collapseWhitespace:true  //合并html空白字符
+
+           }
+       })
+    ],
+    devServer:{
+        hot:true,
+        contentBase:"./dist",
+        port:8080,
+        inline:true
+    }
+}
